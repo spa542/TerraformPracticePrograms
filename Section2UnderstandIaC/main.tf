9278227fcdcecd218aa48302a4c1a362b4c00e90
spa542 <ryan11291129@gmail.com>
2023-08-10 23:16:10 -0400

    Add configuration update

--- Section2UnderstandIaC/main.tf
+++ Section2UnderstandIaC/main.tf
@@ -106,3 +106,27 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "demo_nat_gateway"
   }
 }
+
+# Terraform Data Block - To lookup latest Ubuntu 20.04 AMI image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109447"]
+}
+
+# Terraform Resource Block - To build EC2 instance in public subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  }
+}

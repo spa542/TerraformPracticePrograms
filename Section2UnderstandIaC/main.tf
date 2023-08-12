9cf17323d013ab8574862fa9ba6f4a0381181b22
spa542 <ryan11291129@gmail.com>
2023-08-12 12:30:05 -0400

    Add some section 4 code and examples

--- Section2UnderstandIaC/main.tf
+++ Section2UnderstandIaC/main.tf
@@ -1,7 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
+# ----------Section 2---------
 # Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
@@ -107,26 +104,77 @@ resource "aws_nat_gateway" "nat_gateway" {
   }
 }
 
+# ----------Section 3---------
 # Terraform Data Block - To lookup latest Ubuntu 20.04 AMI image
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+#   owners = ["099720109477"]
+# }
+
+# # Terraform Resource Block - To build EC2 instance in public subnet
+# resource "aws_instance" "web_server" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+#   tags = {
+#     Name = "Ubuntu EC2 Server"
+#   }
+# }
+
+# ----------Section 4---------
+resource "aws_instance" "web" {
+  ami                    = "ami-09538990a0c4fe9be"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnets["public_subnet_1"].id
+  vpc_security_group_ids = ["sg-00ed13934e19ad957"]
+  tags = {
+    "Terraform" = "true"
   }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109447"]
 }
 
-# Terraform Resource Block - To build EC2 instance in public subnet
-resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+resource "aws_s3_bucket" "my-new-S3-bucket" {
+  bucket = "my-new-tf-bucket-ryan-rosi-${random_id.randomness.hex}"
+
   tags = {
-    Name = "Ubuntu EC2 Server"
+    Name    = "My S3 Bucket"
+    Purpose = "Intro to Resource Blocks Lab"
   }
+}
+
+resource "aws_s3_bucket_acl" "my_new_bucket_acl" {
+  bucket = aws_s3_bucket.my-new-S3-bucket.id
+  acl    = "private"
+}
+
+resource "aws_security_group" "my-new-security-group" {
+  name        = "web_server_inbound"
+  description = "Allow inbound traffic on tcp/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "Allow 443 from the Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}
+
+# Will break, need to either run terraform init or change the resource
+# Since the aws provider alone does not have this resource (Terraform will try to find one that will have it)
+resource "random_id" "randomness" {
+  byte_length = 16
 }

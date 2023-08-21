069c39bc6462929882f1c6e4f90d9324dc868210
spa542 <ryan11291129@gmail.com>
2023-08-21 15:03:48 -0400

    Add more backend configuration notes

---
+++ BackendAuthenticationExample/state_configuration/dev_s3_state.hcl
@@ -0,0 +1,2 @@
+bucket = "bucket"
+region = "region"
--- BackendAuthenticationExample/terraform.tf
+++ BackendAuthenticationExample/terraform.tf
@@ -1,16 +1,39 @@
 terraform {
   // Using S3 remote backend
-  backend "s3" {
-    bucket = "my-terraform-state-rcr-sample"
-    key = "sample/aws_infra"
-    region = "us-east-1"
-  }
+  // Commonly used as a standard backend - A standard backend is where the Terraform state file is stored
+  // remotely but the workflow is performed locally (plan/apply/destroy)
+  # backend "s3" {
+  #   bucket = "my-terraform-state-rcr-sample"
+  #   key = "sample/aws_infra"
+  #   region = "us-east-1"
+  #   dynamodb_table = "terraform-locks"
+  #   encrypt = true
+  # }
+  // HTTP Option using a Basic HTTP Server
+  # backend "http" {
+  #   address = "http://localhost:5000/terraform_state/my_state"
+  #   lock_address = "http://localhost:5000/terraform_lock/my_state"
+  #   lock_method = "PUT"
+  #   unlock_address = "http://localhost:5000/terraform_state/my_state"
+  #   unlock_method = "DELETE"
+  # }
   // Cloud option setup in Terraform cloud
-  # cloud {
-  #   organization = "PrivateWorkspace"
+  // You will either need to run Terraform cloud remotely and set your AWS access credentials as 
+  // environment variables on in the workspace OR set your runs to local and only the state file will be stored
+  // in the cloud
+  cloud {
+    organization = "PrivateWorkspace"
 
+    workspaces {
+      name = "FirstCloudWorkspaceTest"
+    }
+  }
+  // Below is an older way to use the Terraform Cloud (Reference)
+  # backend "remote" {
+  #   hostname = "app.terraform.io"
+  #   organization = "Enterprise-Cloud"
   #   workspaces {
-  #     name = "FirstCloudWorkspaceTest"
+  #     name = "my-aws-app"
   #   }
   # }
   // For managing various providers/plugins and their version information
@@ -35,6 +58,9 @@ terraform {
 }
 
 provider "aws" {
+  // Can set shared credentials files for apply
+  # shared_credentials_files = [""]
+  # profile = "default"
   region = "us-east-1"
   default_tags {
     tags = {

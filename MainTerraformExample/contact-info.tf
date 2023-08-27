7de5e4519a1df8c6e7e370b6e7f6974c6363a1d8
spa542 <ryan11291129@gmail.com>
2023-08-27 17:17:13 -0400

    Testing outside configuration VCS

--- MainTerraformExample/contact-info.tf
+++ MainTerraformExample/contact-info.tf
@@ -35,6 +35,7 @@ output "last_name" {
   sensitive = true
 }
 
+// Testing
 // Must be marked as sensitive because the values themselves have been marked as sensitive up above
 output "phone_number" {
   value     = local.contact_info.phone_number
